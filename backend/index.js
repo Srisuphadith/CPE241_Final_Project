aa4550248e527ac1b652b03a313cb51f36312087
FiFaKuBz <fifa.yanakorn@gmail.com>
2025-04-27 01:25:35 +0700

    make login page and include tailwind to project

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,59 @@
 const express = require('express');
 const cors = require('cors');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const secret = process.env.SECRET_KEY;
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+}));
 app.use(express.json());
 
-const db = mysql.createConnection({
+app.use(session({
+    secret: secret,
+    resave: false,
+    saveUninitialized: false,
+}));
+
+const db = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'CPE241_SHOP'
 });
 
-db.connect(err => {
-    if (err) throw err;
-    console.log('MySQL Connected...')
+app.post('/api/login', async(req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const [rows] = await db.execute('SELECT user_ID, firstName, role, password_hash FROM tbl_users WHERE userName = ?', [username]);
+        const user = rows[0];
+
+        if (user && await bcrypt.compare(password, user.password_hash)) {
+            req.session.userID = user.user_ID;
+            req.session.role = user.role;
+            req.session.firstName = user.firstName;
+            res.json({ success: true, message: 'Login successful!' });
+        } else {
+            res.json({ success: false, message: 'Invalid username or password' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: 'Server error' });
+    }
 });
 
-app.get('/api', (req, res) => {
-    res.json({ message: 'Hello From Express!' });
+app.get('/api/market', (req, res) => {
+    if (req.session.userID) {
+        res.json({ message: `Welcome ${req.session.firstName}` });
+    } else {
+        res.status(401).json({ message: 'Unauthorized' });
+    }
 });
 
-app.listen(5000, () => console.log('Server running on Port 5000'))
+app.listen(5000, () => {
+    console.log('Server is running on http://localhost:5000');
+});
