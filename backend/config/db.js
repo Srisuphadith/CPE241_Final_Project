356783974a79dabe6f4b27587e7c6689489880d0
FiFaKuBz <fifa.yanakorn@gmail.com>
2025-04-28 18:49:05 +0700

    fix error in frontend

---
+++ backend/config/db.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2/promise');
+
+const db = mysql.createPool({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'CPE241_SHOP'
+});
+
+module.exports = db;
