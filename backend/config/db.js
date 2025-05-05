e5d264d6a495c39aaffac2d148de1bd554f51bd0
FiFaKuBz <fifa.yanakorn@gmail.com>
2025-05-05 14:51:29 +0700

    update .env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,14 @@
 require('dotenv').config();
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-const connection = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'CPE241_SHOP',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.error('เชื่อมต่อฐานข้อมูลล้มเหลว:', err);
-    } else {
-        console.log('เชื่อมต่อฐานข้อมูลสำเร็จ!');
-    }
-});
-
-module.exports = connection;
+module.exports = db;
