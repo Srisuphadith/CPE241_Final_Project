1d365502e771285217adda791f6e8cfb7503f53a
Fubuki <75804199+Srisuphadith@users.noreply.github.com>
2025-05-07 16:03:40 +0700

    tailwind product

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'db',
-    user: process.env.DB_USER || 'sysadmin',
-    password: process.env.DB_PASSWORD || '123',
+    host: process.env.DB_HOST || 'locahost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'CPE241_SHOP'
         // port: process.env.DB_PORT || '3306'
 });
--- frontend/src/pages/product/components/Header.jsx
+++ frontend/src/pages/product/components/Header.jsx
@@ -1,30 +1,52 @@
 import React from 'react';
 import logo_3 from '../../../assets/img/logo_3.png';
 export default function Header() {
-  return (
-    <header className="flex items-center justify-between p-4 bg-[#1e1a1d]">
-        
-      <div className="flex items-center gap-2">
-        <img src={logo_3} alt="Mongkol Logo" className="w-30 h-20" />
-        <h1 className="text-orange-400 text-3xl font-bold">MONGKOL</h1>
-      </div>
+    return (
+        <header className="flex items-center justify-between p-4 bg-[#1e1a1d]">
 
-      <nav className="flex gap-4 bg-orange-400 p-2 rounded">
-        {['พุทธ', 'คริสต์', 'อิสลาม', 'เทพเจ้า', 'อื่น ๆ'].map((tab) => (
-          <button key={tab} className="text-black font-medium hover:underline">
-            {tab}
-          </button>
-        ))}
-      </nav>
- 
-      <div className="flex items-center gap-2">
-        <input
-          type="text"
-          placeholder="Search"
-          className="p-1 rounded"
-        />
-        <button className="bg-white text-black px-3 py-1 rounded">Order</button>
-      </div>
-    </header>
-  );
+            <div className='w-full grid grid-cols-2 gap-10'>
+                <div className=''>
+                    <div className="flex items-center gap-2">
+                        <img src={logo_3} alt="Mongkol Logo" className="w-30 h-20" />
+                        <h1 className="text-orange-400 text-3xl font-bold">MONGKOL</h1>
+                    </div>
+                </div>
+
+
+                <div className=''>
+                    <div className="flex items-center gap-2">
+                        <input
+                            type="text"
+                            placeholder="Search"
+                            className="p-1 rounded"
+                        />
+                        <button className="bg-white text-black px-3 py-1 rounded">Order</button>
+                    </div>
+                </div>
+                <div className='col-span-2'>
+                    <nav className="flex gap-4 bg-orange-400 p-2 rounded">
+                        {['พุทธ', 'คริสต์', 'อิสลาม', 'เทพเจ้า', 'อื่น ๆ'].map((tab) => (
+                            <button key={tab} className="text-black font-medium hover:text-white">
+                                {tab}
+                            </button>
+                        ))}
+                    </nav>
+                </div>
+
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </header>
+    );
 }
